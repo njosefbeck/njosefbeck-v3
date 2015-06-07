09c4ad37d08ff140ef03df9719e60458728799bc
Nathan Beck <njosefbeck@gmail.com>
2015-06-07 15:14:19 -0500

    Configured states in ui-router

--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,41 @@ angular.module('njosefbeck', ['ngTouch', 'ngSanitize', 'ui.router', 'ui.bootstra
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
+      })
+      .state('devAbout', {
+      	url: 'dev-about',
+      	templateUrl: 'app/dev/devAbout.html',
+      	controller: 'devAboutCtrl'
+      })
+      .state('devContact', {
+      	url: 'dev-contact',
+      	templateUrl: 'app/dev/devContact.html',
+      	controller: 'devContactCtrl'
+      })
+      .state('devProjects', {
+      	url: 'dev-projects',
+      	templateUrl: 'app/dev/devProjects.html',
+      	controller: 'devProjectsCtrl'
+      })
+      .state('spanishAbout', {
+      	url: 'spanish-about',
+      	templateUrl: 'app/spanish/spanishAbout.html',
+      	controller: 'spanishAboutCtrl'
+      })
+      .state('spanishContact', {
+      	url: 'spanish-contact',
+      	templateUrl: 'app/spanish/spanishContact.html',
+      	controller: 'spanishContactCtrl'
+      })
+      .state('yogaAbout', {
+      	url: 'yoga-about',
+      	templateUrl: 'app/yoga/yogaAbout.html',
+      	controller: 'yogaAboutCtrl'
+      })
+      .state('yogaContact', {
+      	url: 'yoga-contact',
+      	templateUrl: 'app/yoga/yogaContact.html',
+      	controller: 'yogaContactCtrl'
       });
 
     $urlRouterProvider.otherwise('/');
