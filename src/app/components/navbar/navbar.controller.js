84c3aa01e40dfd0b893fb763eb463448698f872f
Nathan Beck <njosefbeck@gmail.com>
2015-06-09 10:56:02 -0500

    Attempt to get background-style to dynamically change on state change

--- src/app/components/navbar/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('njosefbeck')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
--- src/app/dev/devAbout-controller.js
+++ src/app/dev/devAbout-controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('njosefbeck')
-  .controller('devAboutCtrl', function ($scope) {
+  .controller('devAboutCtrl', function ($scope, $rootScope) {
+
   });
